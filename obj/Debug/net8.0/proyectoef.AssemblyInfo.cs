0d719e8c6f68b78bac9cfb8f005dc3d03dd69d8e
Owen Roque <rokefeler@gmail.com>
2024-07-25 20:34:58 -0500

    Culminado

--- obj/Debug/net8.0/proyectoef.AssemblyInfo.cs
+++ obj/Debug/net8.0/proyectoef.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("proyectoef")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fa512b7de313efb8d8fdc34135edd13572af73c")]
 [assembly: System.Reflection.AssemblyProductAttribute("proyectoef")]
 [assembly: System.Reflection.AssemblyTitleAttribute("proyectoef")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
